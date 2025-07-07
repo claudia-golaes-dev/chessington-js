0481c8400577c5c0d3bdefe16173ba1377217f06
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-07 15:32:27 +0300

    messy code

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,60 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Array<Square> = new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        let forwardsDiagonalRowPosition = 0;
+        let backwardsDiagonalRowPosition = 0;
+
+        let forwardsDiagonalColPosition = currentSquare.col - currentSquare.row;
+        let backwardsDiagonalColPosition = currentSquare.col + currentSquare.row;
+        let forwardsDiagonal:Square;
+        let backwardsDiagonal:Square;
+
+        let newRowPosition = currentSquare.row;
+        let newColPosition = currentSquare.col;
+        let horizontalPosition:Square;
+        let verticalPosition:Square;
+
+        for (let i = 0; i < 8 ; i++){
+            if(i != newColPosition) {
+                horizontalPosition = Square.at(newRowPosition, i);
+                availableMoves.push(horizontalPosition);
+            }
+            if(i != newRowPosition) {
+                verticalPosition = Square.at(i, newColPosition);
+                availableMoves.push(verticalPosition);
+            }
+        }
+
+        while (forwardsDiagonalColPosition < 0) {
+            forwardsDiagonalColPosition++;
+            forwardsDiagonalRowPosition++;
+        }
+        while (backwardsDiagonalColPosition > 7) {
+            backwardsDiagonalColPosition--;
+            backwardsDiagonalRowPosition++;
+        }
+
+        while (forwardsDiagonalColPosition <= 7 && forwardsDiagonalRowPosition <= 7) {
+            if(forwardsDiagonalRowPosition != currentSquare.row) {
+                forwardsDiagonal = Square.at(forwardsDiagonalRowPosition, forwardsDiagonalColPosition);
+                availableMoves.push(forwardsDiagonal);
+            }
+            forwardsDiagonalColPosition++;
+            forwardsDiagonalRowPosition++;
+        }
+
+        while (backwardsDiagonalRowPosition <= 7 && backwardsDiagonalColPosition >= 0) {
+            if(backwardsDiagonalRowPosition != currentSquare.row) {
+                backwardsDiagonal = Square.at( backwardsDiagonalRowPosition, backwardsDiagonalColPosition);
+                availableMoves.push(backwardsDiagonal);
+            }
+            backwardsDiagonalColPosition--;
+            backwardsDiagonalRowPosition++;
+        }
+
+        return availableMoves;
     }
 }
