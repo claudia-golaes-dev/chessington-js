b2f8e12251f7663685ba7ff4f86460deeeb3cf37
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-07 15:09:04 +0300

    Solved rook move

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Array<Square> = new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        let newRowPosition = currentSquare.row;
+        let newColPosition = currentSquare.col;
+        let horizontalPosition:Square;
+        let verticalPosition:Square;
+
+        for (let i = 0; i < 8 ; i++){
+            if(i != newColPosition) {
+                horizontalPosition = Square.at(newRowPosition, i);
+                availableMoves.push(horizontalPosition);
+            }
+            if(i != newRowPosition) {
+                verticalPosition = Square.at(i, newColPosition);
+                availableMoves.push(verticalPosition);
+            }
+        }
+        return availableMoves;
     }
 }
