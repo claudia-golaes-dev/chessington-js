711a29ca6aa85c4292e0f58db921c689391c62cb
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-07 15:27:38 +0300

    solved the bishop moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,44 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Array<Square> = new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        let forwardsDiagonalRowPosition = 0;
+        let backwardsDiagonalRowPosition = 0;
+
+        let forwardsDiagonalColPosition = currentSquare.col - currentSquare.row;
+        let backwardsDiagonalColPosition = currentSquare.col + currentSquare.row;
+        let forwardsDiagonal:Square;
+        let backwardsDiagonal:Square;
+
+        while (forwardsDiagonalColPosition < 0) {
+            forwardsDiagonalColPosition++;
+            forwardsDiagonalRowPosition++;
+        }
+        while (backwardsDiagonalColPosition > 7) {
+            backwardsDiagonalColPosition--;
+            backwardsDiagonalRowPosition++;
+        }
+
+        while (forwardsDiagonalColPosition <= 7 && forwardsDiagonalRowPosition <= 7) {
+            if(forwardsDiagonalRowPosition != currentSquare.row) {
+                forwardsDiagonal = Square.at(forwardsDiagonalRowPosition, forwardsDiagonalColPosition);
+                availableMoves.push(forwardsDiagonal);
+            }
+            forwardsDiagonalColPosition++;
+            forwardsDiagonalRowPosition++;
+        }
+
+        while (backwardsDiagonalRowPosition <= 7 && backwardsDiagonalColPosition >= 0) {
+            if(backwardsDiagonalRowPosition != currentSquare.row) {
+                backwardsDiagonal = Square.at( backwardsDiagonalRowPosition, backwardsDiagonalColPosition);
+                availableMoves.push(backwardsDiagonal);
+            }
+            backwardsDiagonalColPosition--;
+            backwardsDiagonalRowPosition++;
+        }
+
+        return availableMoves;
     }
 }
